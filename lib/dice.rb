17f12bf0ff8ee5de4bcd86cade3a296247d45c9a
James Sutherland <jrsutherland78@googlemail.com>
2018-08-14 18:54:35 +0100

    Completed user requirements and rspec tests

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,20 @@
 class Dice
-  def roll
-    1
+
+  attr_reader :roll_storage, :turn_storage
+
+  def initialize
+    @roll_storage = []
+    @turn_storage = []
   end
+
+  def roll(dice_rolls = 1)
+    @roll_storage = []
+    dice_rolls.times { @roll_storage << random_return }
+    turn_storage << @roll_storage.inject(:+)
+  end
+
+  def random_return
+    rand(1..6)
+  end
+
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,9 +1,22 @@
 require 'dice'
 
 describe Dice do
+
   it {expect(subject).to respond_to :roll}
 
   it 'rolls a number from 1 to 6' do
-  expect(subject.roll).to be_between(1, 6).inclusive
+    expect(subject.random_return).to be_between(1, 6).inclusive
+  end
+
+  it 'roll results are stored in an array' do
+    allow(subject).to receive(:rand) { 6 }
+    subject.roll
+    expect(subject.roll_storage).to eq [6]
+  end
+
+  it 'roll results are stored in an array' do
+    allow(subject).to receive(:rand) { 6 }
+    subject.roll(2)
+    expect(subject.roll_storage).to eq [6, 6]
   end
 end
